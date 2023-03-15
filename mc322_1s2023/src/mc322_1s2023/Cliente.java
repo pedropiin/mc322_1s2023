de870ea26343c2dcf2484c4061278f1eb88822d0
pedropiin <pedarosa04@gmail.com>
2023-03-15 15:25:39 -0300

    constructors e funcoes feitas

--- mc322_1s2023/src/mc322_1s2023/Cliente.java
+++ mc322_1s2023/src/mc322_1s2023/Cliente.java
@@ -1,5 +1,57 @@
 package mc322_1s2023;
 
 public class Cliente {
-
+	private int idade;
+	private String nome;
+	private String cpf;
+	private String dataNascimento;
+	private String endereco;
+	
+	public Cliente(int idade, String nome, String cpf, String dataNascimento, String endereco) {
+		this.idade = idade;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+		this.endereco = endereco;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento; 
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
 }
--- mc322_1s2023/src/mc322_1s2023/Sinistro.java
+++ mc322_1s2023/src/mc322_1s2023/Sinistro.java
@@ -1,9 +1,42 @@
 package mc322_1s2023;
 
+import java.util.Random;
+
 public class Sinistro {
 	
 	private int id;
-	private Sinistro() {
-		
+	private String data;
+	private String endereco;
+	
+	private Sinistro(String data, String endereco) {
+		this.data = data;
+		this.endereco = endereco;
+		this.id = geraId();
+	}
+	
+	public int geraId() {
+		Random rand = new Random();
+		int limite = 999999999;
+		return rand.nextInt(limite);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getData() {
+		return data;
+	}
+	
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
 	}
 }
